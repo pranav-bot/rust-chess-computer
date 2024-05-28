3b0c081f9cb086741df97f262631a8254304a626
pranav <pranav23advani@gmail.com>
2024-05-28 20:02:08 +0530

    added enums and structs to represent the board

--- src/main.rs
+++ src/main.rs
@@ -23,22 +23,70 @@ fn index_to_position(index: usize) -> String {
     return format!("{}{}", COL_MAP[column], row);
 }
 
-fn bit_scan(mut bit: u64) -> usize {
-    let mut leading_zeroes = 0;
-    while bit & 1 == 0 {
-        bit >>= 1;
-        leading_zeroes += 1;
-    }
-    return leading_zeroes;
+static MOD67TABLE: [usize; 67] = [
+    64, 0, 1, 39, 2, 15, 40, 23,
+    3, 12, 16, 59, 41, 19, 24, 54,
+    4, 64, 13, 10, 17, 62, 60, 28,
+    42, 30, 20, 51, 25, 44, 55, 47,
+    5, 32, 64, 38, 14, 22, 11, 58,
+    18, 53, 63, 9, 61, 27, 29, 50,
+    43, 46, 31, 37, 21, 57, 52, 8,
+    26, 49, 45, 36, 56, 7, 48, 35,
+    6, 34, 33];
+
+fn bit_scan(bit: u64) -> usize {
+    let remainder: usize = (bit % 67) as usize;
+    return MOD67TABLE[remainder];
+}
+
+#[derive(Debug, PartialEq)]
+enum Color {
+    White,
+    Black
+}
+
+#[derive(Debug, PartialEq)]
+enum PieceType {
+    Pawn,
+    Rook,
+    Knight,
+    Bishop,
+    Queen,
+    King
+}
+
+
+#[derive(Debug, PartialEq)]
+struct Piece {
+    position: PiecePosition,
+    color: Color,
+    piece_type: PieceType
+}
+
+#[derive(Debug)]
+enum Square {
+    Empty,
+    Occupied(usize)
+}
+
+struct Game {
+    pieces: Vec<Piece>,
+    squares: Vec<Square>,
+    
 }
 
 fn main() {
     for i in 0..64 {
         println!("{} -> {}", i, index_to_position(i));
     }
-    for i in 0..10 {
-        println!("{} -> {}", i, 1<<i)
+    for i in 0..63 {
+        let bitstring = (1 as u64) <<i;
+        let calculated_index = bit_scan(bitstring); 
+        if calculated_index!=i {
+            println!("Error at {}", i);
+        }
     }
+    println!("Done");
 }
 
 
