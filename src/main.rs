2efd93328478440626bd5effa6e0773baaccdcad
pranav <pranav23advani@gmail.com>
2024-05-30 07:26:08 +0530

    Completed board representation

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,15 @@
 
 type PiecePosition = u64;
 
-fn bit_position(bit: PiecePosition) -> Result<String, String>{
-    if bit == 0 {
-        return Err("No piece present".to_string());
-    }
-    else {
-        let onebit_index = bit_scan(bit);
-        return Ok(index_to_position(onebit_index));
-    }
-}
+// fn bit_position(bit: PiecePosition) -> Result<String, String>{
+//     if bit == 0 {
+//         return Err("No piece present".to_string());
+//     }
+//     else {
+//         let onebit_index = bit_scan(bit);
+//         return Ok(index_to_position(onebit_index));
+//     }
+// }
 
 static COL_MAP: [char;8] = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
 
@@ -23,7 +23,18 @@ fn index_to_position(index: usize) -> String {
     return format!("{}{}", COL_MAP[column], row);
 }
 
-static MOD67TABLE: [usize; 67] = [
+// fn bit_scan_simple(mut bit: u64) -> usize {
+//     let mut leading_zeros = 0;
+//     while bit & 1 == 0 {
+//         bit >>= 1;
+//         leading_zeros += 1;
+//     }
+
+//     return leading_zeros;
+// }
+
+
+static _MOD67TABLE: [usize; 67] = [
     64, 0, 1, 39, 2, 15, 40, 23,
     3, 12, 16, 59, 41, 19, 24, 54,
     4, 64, 13, 10, 17, 62, 60, 28,
@@ -34,12 +45,12 @@ static MOD67TABLE: [usize; 67] = [
     26, 49, 45, 36, 56, 7, 48, 35,
     6, 34, 33];
 
-fn bit_scan(bit: u64) -> usize {
+fn _bit_scan(bit: u64) -> usize {
     let remainder: usize = (bit % 67) as usize;
-    return MOD67TABLE[remainder];
+    return _MOD67TABLE[remainder];
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone, Copy)]
 enum Color {
     White,
     Black
@@ -71,22 +82,96 @@ enum Square {
 
 struct Game {
     pieces: Vec<Piece>,
-    squares: Vec<Square>,
-    
+    squares: Vec<Square>,   
+}
+
+impl Game {
+
+    fn push_piece_and_square(&mut self, position: usize, color: Color, piece_type: PieceType, index: &mut usize) {
+        self.pieces.push(Piece {position: (1 as u64)<<position, color: color, piece_type});
+        self.squares.push(Square::Occupied(*index));
+        *index +=1;
+    }
+
+    fn push_empty_square(&mut self) {
+        self.squares.push(Square::Empty)
+    }
+
+    fn initialize() -> Game {
+        let mut game = Game {pieces: vec![], squares: vec![]};
+        let mut piece_index = 0;
+        let color = Color::White;
+        game.push_piece_and_square(0, color, PieceType::Rook, &mut piece_index);
+        game.push_piece_and_square(1, color, PieceType::Knight, &mut piece_index);
+        game.push_piece_and_square(2, color, PieceType::Bishop, &mut piece_index);
+        game.push_piece_and_square(3, color, PieceType::Queen, &mut piece_index);
+        game.push_piece_and_square(4, color, PieceType::King, &mut piece_index);
+        game.push_piece_and_square(5, color, PieceType::Bishop, &mut piece_index);
+        game.push_piece_and_square(6, color, PieceType::Knight, &mut piece_index);
+        game.push_piece_and_square(7, color, PieceType::Rook, &mut piece_index);
+        for i in 8..16 {
+            game.push_piece_and_square(i, color, PieceType::Pawn, &mut piece_index);
+        }
+        for _ in 16..48 {
+            game.push_empty_square();
+        }
+        let color = Color::Black;
+        for i in 48..56 {
+            game.push_piece_and_square(i, color, PieceType::Pawn, &mut piece_index);
+        }
+        let offset = 56;
+        game.push_piece_and_square(0+offset, color, PieceType::Rook, &mut piece_index);
+        game.push_piece_and_square(1+offset, color, PieceType::Knight, &mut piece_index);
+        game.push_piece_and_square(2+offset, color, PieceType::Bishop, &mut piece_index);
+        game.push_piece_and_square(3+offset, color, PieceType::Queen, &mut piece_index);
+        game.push_piece_and_square(4+offset, color, PieceType::King, &mut piece_index);
+        game.push_piece_and_square(5+offset, color, PieceType::Bishop, &mut piece_index);
+        game.push_piece_and_square(6+offset, color, PieceType::Knight, &mut piece_index);
+        game.push_piece_and_square(7+offset, color, PieceType::Rook, &mut piece_index);
+
+        game
+    }
+
+    fn to_string(&self) -> String {
+        let mut board = "".to_owned();
+        let mut temp = "".to_owned();
+
+        for (i, square) in self.squares.iter().enumerate() {
+            match square {
+                Square::Empty => temp.push_str(&index_to_position(i)),
+                Square::Occupied(idx) => temp.push_str(&self.pieces[*idx].to_string()),
+            }
+            if (i +1) % 8 == 0{
+                temp.push_str("\n");
+                board.insert_str(0, &temp);
+                temp.clear();
+            }
+        }
+        board.insert_str(0, &temp);
+        board
+    }
+}
+
+impl Piece {
+    fn to_string(&self) -> String {
+        let mut result = match self.piece_type {
+            PieceType::Pawn => "p ",
+            PieceType::Rook => "r ",
+            PieceType::Knight => "n ",
+            PieceType::Bishop => "b ",
+            PieceType::Queen => "q ",
+            PieceType::King => "k ",
+        }.to_string();
+        if self.color == Color::White {
+            result.make_ascii_uppercase();
+        }
+        result.to_ascii_uppercase()
+    }
 }
 
 fn main() {
-    for i in 0..64 {
-        println!("{} -> {}", i, index_to_position(i));
-    }
-    for i in 0..63 {
-        let bitstring = (1 as u64) <<i;
-        let calculated_index = bit_scan(bitstring); 
-        if calculated_index!=i {
-            println!("Error at {}", i);
-        }
-    }
-    println!("Done");
+    let game = Game::initialize();
+    println!("{}", game.to_string());
 }
 
 
